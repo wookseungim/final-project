9733d13aa974d6d67869d784e2aa6c5476c7a2bf
wookseungim <wookseungim@naver.com>
2020-12-07 10:38:44 +0900

    final test upload

--- build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -7,11 +7,11 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH='/home/pi/catkin_ws/devel/.private/catkin_tools_prebuild:/opt/ros/melodic:/home/pi/catkin_ws/devel'
-export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/home/pi/catkin_ws/devel/lib'
+export CMAKE_PREFIX_PATH="/home/pi/catkin_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib'
 export PATH='/opt/ros/melodic/bin:/home/pi/.pyenv/plugins/pyenv-virtualenv/shims:/home/pi/.pyenv/shims:/home/pi/.pyenv/bin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games'
-export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig:/home/pi/catkin_ws/devel/lib/pkgconfig'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig'
 export PWD='/home/pi/catkin_ws/build/catkin_tools_prebuild'
-export PYTHONPATH='/opt/ros/melodic/lib/python2.7/dist-packages:/home/pi/catkin_ws/devel/lib/python2.7/dist-packages'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/pi/catkin_ws/devel/.private/catkin_tools_prebuild/share/common-lisp'
-export ROS_PACKAGE_PATH='/home/pi/catkin_ws/build/catkin_tools_prebuild:/opt/ros/melodic/share'
+export PYTHONPATH='/opt/ros/melodic/lib/python2.7/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/pi/catkin_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/pi/catkin_ws/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
